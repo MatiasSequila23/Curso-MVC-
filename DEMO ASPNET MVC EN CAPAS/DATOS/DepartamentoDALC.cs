5552511b495ac9c51c9b22aa1d08b7b0aef5c195
Matias Sequila <martinmatiassequila@outlook.es>
2023-03-27 21:17:57 -0300

    Add proyecto general

---
+++ DEMO ASPNET MVC EN CAPAS/DATOS/DepartamentoDALC.cs
@@ -0,0 +1,54 @@
+﻿using ENTIDAD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATOS
+{
+    public class DepartamentoDALC
+    {
+        public void Agregar(Departamento dpto) 
+        {
+            using (var db = new PROYECTOSDBEntities ())
+            {
+                db.Departamento.Add(dpto);
+                db.SaveChanges();
+            }
+        } 
+        public List<Departamento> ListarDepartamentos()
+        {
+            using (var db= new PROYECTOSDBEntities())
+            {
+                return db.Departamento.ToList();
+            }
+        }
+        public Departamento GetDepartamento(int id) 
+        {
+            using (var db = new PROYECTOSDBEntities())
+            {
+                //return db.Departamento.Find;
+                return db.Departamento.Where(d => d.DepartamentoId == id).FirstOrDefault();
+            }
+        }
+        public void Editar(Departamento dpto)
+        {
+            using (var db = new PROYECTOSDBEntities())
+            {
+                var d = db.Departamento.Find(dpto.DepartamentoId);
+                d.NombreDepartamento = dpto.NombreDepartamento;
+                db.SaveChanges();
+            }
+        }
+        public void Eliminar(int id)
+        {
+            using (var db = new PROYECTOSDBEntities())
+            {
+                var dpto = db.Departamento.Find(id);
+                db.Departamento.Remove(dpto);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DEMO ASPNET MVC EN CAPAS/WEB_PROYECTOS/Controllers/DepartamentoController.cs
@@ -0,0 +1,99 @@
+﻿using ENTIDAD;
+using NEGOCIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB_PROYECTOS.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        // GET: Departamento
+        public ActionResult Index()
+        {
+            var dptos= DepartamentoCN.ListarDepartamentos();
+            return View(dptos);
+        }
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Crear(Departamento dpto)
+        {
+            try
+            {
+                if (dpto.NombreDepartamento==null)
+                {
+                    ModelState.AddModelError("", "Debe ingresar un nombre de un departamento.");
+                    return View(dpto);
+                }
+                DepartamentoCN.Agregar(dpto);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("","Ocurrio un error al agregar un Departamento");
+                return View(dpto);
+            }
+        }
+
+        public ActionResult GetDepartamento(int id)
+        {
+            var dpto = DepartamentoCN.GetDepartamento(id); 
+            return View(dpto);
+        }
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            var dpto = DepartamentoCN.GetDepartamento(id);
+            return View(dpto);
+        }
+        [HttpPost]
+        public ActionResult Editar(Departamento dpto)
+        {
+            try
+            {
+                if (dpto.NombreDepartamento == null)
+                {
+                    ModelState.AddModelError("", "Debe ingresar un nombre de un departamento.");
+                    return View(dpto);
+                }
+                DepartamentoCN.Editar(dpto);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Ocurrio un error al editar un Departamento");
+                return View(dpto);
+            }
+        }
+        [HttpGet]
+        public ActionResult Eliminar(int? id)
+        {
+            if (id== null)
+               return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            var dpto = DepartamentoCN.GetDepartamento(id.Value);
+            return View(dpto);
+        }
+        [HttpPost]
+        public ActionResult Eliminar(int id)
+        {
+            try
+            {
+                DepartamentoCN.Eliminar(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Ocurrio un error al eliminar un Departamento");
+                return View();
+            }
+        }
+    }
+}
